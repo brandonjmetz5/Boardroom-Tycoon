aacbee1c30a973354aaf548a6037d2c0d0c74b1c
Brandon Metz <Brandonjmetz5@gmail.com>
2026-03-11 10:46:15 -0400

    Initial Commit

---
+++ Boardroom Tycoon/Boardroom_TycoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boardroom_TycoonApp.swift
+//  Boardroom Tycoon
+//
+//  Created by brandon metz on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Boardroom_TycoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
